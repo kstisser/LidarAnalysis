28c92f4d0d2ec9e3921fe0eb8882fcc4a609e643
Rahul Dev <rappapogu@dekaresearch.com>
2020-11-10 12:52:15 -0500

    Adding ros to temp humidity python script

--- SenseHat/sensehat.py
+++ SenseHat/sensehat.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python
 from sense_hat import SenseHat
 from time import sleep
 import numpy as np
+import rospy
+from std_msgs.msg import Float32
 
 #Purpose of this class is to read temperature and humidity readings from a 
 #raspberry pi sense hat
@@ -8,6 +11,7 @@ import numpy as np
 class SensorReadings:
     def __init__(self):
         self.sense = SenseHat()
+        self.sense.clear()
         self.temperatures = np.array([])
         self.humidities = np.array([])
 
@@ -36,3 +40,19 @@ class SensorReadings:
             self.humidities = np.append(self.humidities, humid)
             print("Humidities moving average: ")
             print(self.humidities.mean())
+
+if __name__ == '__main__':
+    try:
+        sr = SensorReadings()
+        tempPub = rospy.Publisher('temperature', Float32, queue_size=10)
+        humidPub = rospy.Publisher('humidity', Float32, queue_size=10)
+
+        rospy.init_node('tempHumid', anonymous=True)
+        rate = rospy.Rate(10) #10 Hz
+
+        while not rospy.is_shutdown():
+            tempPub.publish(sr.getTemperature())
+            humidPub.publish(sr.getHumidity())
+            rate.sleep() 
+    except rospy.ROSInterruptException:
+        pass
