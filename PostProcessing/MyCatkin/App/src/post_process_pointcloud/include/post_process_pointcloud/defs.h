1e10e2636718a9a287a156242dc57edef34f7ee1
Ari <astisser>
2020-11-13 16:56:44 -0500

    Adding scenario enums to C++

--- PostProcessing/MyCatkin/App/src/post_process_pointcloud/include/post_process_pointcloud/defs.h
+++ PostProcessing/MyCatkin/App/src/post_process_pointcloud/include/post_process_pointcloud/defs.h
@@ -8,3 +8,47 @@ namespace Topics
    std::string IRimageTopic = Sensor0Prefix + "/Infrared_0/image/data";
    std::string PointCloud = Sensor0Prefix + "/Depth_0/image/data";
 }
+
+namespace Scenarios
+{
+   enum Size
+   {
+      MM40,
+      MM80,
+      MM160,
+      NAsize
+   };
+
+   enum ObjectTemperature
+   {
+      C40,
+      C45,
+      C50,
+      Ambient,
+      NAtemp
+   };
+
+   enum ObjectColor
+   {
+      Black,
+      RoseGold,
+      Blue,
+      Silver,
+      White
+   };
+
+   enum Distance
+   {
+      FT2,
+      FT4,
+      FT6
+   };
+
+   struct scenario
+   {
+      Size size;
+      ObjectTemperature temperature;
+      ObjectColor color;
+      Distance distance;
+   };
+}
