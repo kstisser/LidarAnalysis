d020ca8bcf6f1e0c31bc49b4c2649802900c4f26
Karissa Stisser <me@example.com>
2020-11-30 23:01:48 -0500

    Updating with number of lidar points

--- Analysis/combineCode.py
+++ Analysis/combineCode.py
@@ -7,6 +7,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
+import scipy.interpolate as interpolate
 
 def traverseDirectoriesAndProcess(filepath):
     masterJSON = {}
@@ -19,8 +20,11 @@ def traverseDirectoriesAndProcess(filepath):
     masterJSON['temperature'] = []
     masterJSON['humidity'] = []
     masterJSON['light'] = []
-    print(filepath)
+    masterJSON['objectTemperature'] = []
+    masterJSON['numLidarPoints'] = []
     filepath = 'C:/Users/fm873/Documents/Repos/LidarAnalysis/Results'
+    print(filepath)
+    
     for root, subdirs, files in os.walk(filepath):
         #print files
         for f in files:
@@ -30,19 +34,42 @@ def traverseDirectoriesAndProcess(filepath):
                     jsonData = json.load(fd)
 
                 print(root)
+                print(np.mean(jsonData['pointNumberByFrame']))
                 #make all fields in the file the same length so they can be added to a data frame
+                pointNumberSize = len(jsonData['pointNumberByFrame'])
+                
                 tempSize = len(jsonData['temperature'])
                 humidSize = len(jsonData['humidity'])
                 lightSize = len(jsonData['light'])
+                
+                '''print("num points")
+                print(pointNumberSize)
+                print("temp size")
+                print(tempSize)
+                print("humid size")
+                print(humidSize)
+                print("light size")
+                print(lightSize)'''
 
                 minVal = min(tempSize, humidSize)
                 diff = abs(tempSize - humidSize)
-                if minVal != 0:
-                    maxLength = minVal
-                else:
-                    maxLength = max([tempSize,humidSize,lightSize])
+                #if minVal != 0:
+                #    maxLength = minVal
+                #else:
+                maxLength = max([tempSize,humidSize,lightSize,pointNumberSize])
                 
                 newTemp = {}
+                newHumid = {}
+                newLight = {}
+                newPoints = {}
+                
+                #interpolate data based on number of points
+                #f = interpolate.interp1d(jsonData['temperature'], jsonData['pointNumberByFrame'])
+                minTemp = min(jsonData['temperature'])
+                maxTemp = max(jsonData['temperature'])
+                #inttemp = np.arrange
+                #intFrameNum
+                
                 if len(jsonData['temperature']) > maxLength:
                     newTemp = (jsonData['temperature'])[:maxLength]
                 else:
@@ -50,7 +77,6 @@ def traverseDirectoriesAndProcess(filepath):
                     while(len(newTemp) < maxLength):
                         newTemp.append(0)
 
-                newHumid = {}
                 if len(jsonData['humidity']) > maxLength:
                     newHumid = (jsonData['humidity'])[:maxLength]
                 else:
@@ -58,16 +84,22 @@ def traverseDirectoriesAndProcess(filepath):
                     while(len(newHumid) < maxLength):
                         newHumid.append(0)
 
-                newLight = {}
                 if len(jsonData['light']) > maxLength:
                     newLight = (jsonData['light'])[:maxLength]
                 else:
                     newLight = (jsonData['light'])
                     while(len(newLight) < maxLength):
                         newLight.append(0)
+                        
+                if len(jsonData['pointNumberByFrame']) > maxLength:
+                    newPoints = (jsonData['pointNumberByFrame'])[:maxLength]
+                else:
+                    newPoints = (jsonData['pointNumberByFrame'])
+                    while(len(newPoints) < maxLength):
+                        newPoints.append(0)        
 
-                print("Max:")
-                print(maxLength)
+                #print("Max:")
+                #print(maxLength)
                 #print("temp size:")
                 #print(tempSize)
                 #print("humid size:")
@@ -92,6 +124,8 @@ def traverseDirectoriesAndProcess(filepath):
                 redList = [redPixel] * maxLength
                 blueList = [bluePixel] * maxLength
                 greenList = [greenPixel] * maxLength
+                
+                pointNum = jsonData["pointNumberByFrame"]
 
                 newJsonData = {}
                 newJsonData["area"] = areaList
@@ -103,6 +137,7 @@ def traverseDirectoriesAndProcess(filepath):
                 newJsonData["temperature"] = newTemp
                 newJsonData["humidity"] = newHumid
                 newJsonData["light"] = newLight
+                newJsonData["numLidarPoints"] = pointNum
 
                 for i in range(maxLength):
                     masterJSON["area"].append(newJsonData["area"][i])
@@ -114,21 +149,40 @@ def traverseDirectoriesAndProcess(filepath):
                     masterJSON["temperature"].append(newJsonData["temperature"][i])
                     masterJSON["humidity"].append(newJsonData["humidity"][i])
                     masterJSON["light"].append(newJsonData["light"][i])
+                    masterJSON["numLidarPoints"].append(newJsonData["numLidarPoints"][i])
 
 
                 #with open((os.path.join(root,'sameSizeAllData.json')), 'w') as fd:
                 #    json.dump(newJsonData, fd)
+    return masterJSON
 
+def getDataFrame(dictionary):
+    dataFrame = pd.DataFrame.from_dict(dictionary)
+    return dataFrame
+                
+def getNormalizedData(masterJSON):                
+    #populate with 0 to 1 values for the masterJSON lib
+    zeroToOne = {}
+    for key in masterJSON:
+        zeroToOne[key] = []
+        maxVal = max(masterJSON[key])
+        for i in range(len(masterJSON[key])):
+            zeroToOne[key].append(masterJSON[key][i]/maxVal)
+    return zeroToOne
+    '''
     #with open((os.path.join(filepath, 'allData.json')), 'w') as fd:
     #    json.dump(masterJSON, fd)
-    dataFrame = pd.DataFrame.from_dict(masterJSON)
+    dataFrame = pd.DataFrame.from_dict(zeroToOne)
     #dataFrame = dataFrame.cumsum()
     #plt.figure()
     #dataFrame.plot()
-    sns.pairplot(dataFrame)
+    #sns.pairplot(dataFrame, hue='red')
+    #sns.replot(data=masterJSON, x="area",
+    g = sns.PairGrid(dataFrame)
+    g.map(sns.scatterplot)'''
+    
 
-
-if __name__ == '__main__':
+'''if __name__ == '__main__':
     if(len(sys.argv) < 2):
         print "Error! Enter base file path as an argument you'd like to traverse to process json files"
     else:
@@ -137,4 +191,4 @@ if __name__ == '__main__':
             print "Error, your directory doesn't seem to exist!"
         else:
             print "Traversing directory"
-            traverseDirectoriesAndProcess(filepath)
+            traverseDirectoriesAndProcess(filepath)'''
